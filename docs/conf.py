c4e497c6239ac3e477798b80d41a3a5ddcb0296e
Gregor Boehl <admin@gregorboehl.com>
2023-02-17 16:18:41 +0100

    nicer docs

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
@@ -10,15 +10,21 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+from grgrjax import __version__
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+
 # -- Project information -----------------------------------------------------
 
 project = 'grgrjax'
-copyright = '2023, Gregor Boehl'
+copyright = '2022, Gregor Boehl'
 author = 'Gregor Boehl'
+
+# The full version, including alpha/beta/rc tags
+version = __version__
+release = version
 
 
 # -- General configuration ---------------------------------------------------
@@ -26,43 +32,63 @@ author = 'Gregor Boehl'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
+# extensions = [
+# "sphinx.ext.autodoc",
+# ]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+    "IPython.sphinxext.ipython_console_highlighting",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates", "**.ipynb_checkpoints"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# version = '0.1.5'
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_theme = "sphinx_book_theme"
+html_copy_source = True
+html_show_sourcelink = True
+html_sourcelink_suffix = ""
+html_title = "grgrjax"
+html_static_path = ["_static"]
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/gboehl/grgrjax",
+    "repository_branch": "main",
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "notebook_interface": "classic",
+    },
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_download_button": True,
+    "description": "Some generic tools for JAX",
+    "github_repo": "grgrjax",
+    "github_user": "gboehl",
+    "sidebar_collapse": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-# autodoc_mock_imports = ['econsieve', 'particles', 'numpy', 'pandas', 'emcee', 'kombine', 'pathos']
+autoclass_content = "both"
+autodoc_member_order = "groupwise"
+latex_use_parts = False
 
 
 def setup(app):
-    app.add_stylesheet('css/custom.css')
+    app.add_css_file('css/custom.css')
 
 
 master_doc = 'index'
-
-html_theme_options = {
-    # 'github_banner': True,
-    'description': 'Some generic tools for JAX',
-    'github_button': True,
-    'github_repo': 'grgrjax',
-    'github_user': 'gboehl',
-    'sidebar_collapse': False,
-}
