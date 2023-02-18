f387fe265ce6ed0cf036bc736695d4d0793aee46
Gregor Boehl <admin@gregorboehl.com>
2023-02-18 15:05:14 +0100

    test doc

--- grgrjax/__init__.py
+++ grgrjax/__init__.py
@@ -3,8 +3,8 @@
 import jax
 import jax.numpy as jnp
 
-from .helpers import *
-from .newton import *
+from .helpers import jvp_vmap, vjp_vmap, val_and_jacfwd, val_and_jacrev
+from .newton import callback_func, newton_jax, newton_jax_jit
 from jax.experimental.host_callback import id_print as jax_print
 amax = jax.jit(lambda x: jnp.abs(x).max())
 
