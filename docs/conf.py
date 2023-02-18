ebbda6006301d3307a34abdd73e0204ee4bb7cc0
Gregor Boehl <admin@gregorboehl.com>
2023-02-18 22:25:06 +0100

    prepare versioning

--- docs/conf.py
+++ docs/conf.py
@@ -10,12 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-from grgrjax import __version__
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('.'))
 # must be called AFTER the above:
+from grgrjax import __version__
 
 
 # -- Project information -----------------------------------------------------
