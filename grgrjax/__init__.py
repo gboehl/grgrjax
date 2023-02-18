65e6a27bff96618c1a746e22f224acb3c429cc10
Gregor Boehl <admin@gregorboehl.com>
2023-02-18 15:14:58 +0100

    test doc

--- grgrjax/__init__.py
+++ grgrjax/__init__.py
@@ -3,9 +3,24 @@
 import jax
 import jax.numpy as jnp
 
-from .helpers import jvp_vmap, vjp_vmap, val_and_jacfwd, val_and_jacrev
-from .newton import callback_func, newton_jax, newton_jax_jit
+from .helpers import (
+    jvp_vmap as jvp_vmap,
+    vjp_vmap as vjp_vmap,
+    val_and_jacfwd as val_and_jacfwd,
+    val_and_jacrev as val_and_jacrev
+)
+from .newton import (
+    callback_func as callback_func,
+    newton_jax as newton_jax,
+    newton_jax_jit as newton_jax_jit
+)
 from jax.experimental.host_callback import id_print as jax_print
-amax = jax.jit(lambda x: jnp.abs(x).max())
+
+
+def amax(x):
+    """return the maximum absolute value
+    """
+    return jnp.abs(x).max()
+
 
 __version__ = '0.1.1'
